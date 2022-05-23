3df7b414767407d88b679f8dd747846eae55a633
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-05-23 15:16:31 -0500

    Setup bundle and linter
    new file:   .github/workflows/linters.yml
    new file:   .gitignore
    new file:   .rspec
    new file:   .rubocop.yml
    new file:   .travis.yml
    new file:   Gemfile
    new file:   Gemfile.lock
    new file:   LICENSE.txt
    modified:   README.md
    new file:   Rakefile
    new file:   bin/console
    new file:   bin/setup
    new file:   catalog-of-things.gemspec
    new file:   lib/catalog/of/things.rb
    new file:   lib/catalog/of/things/version.rb
    new file:   spec/catalog/of/things_spec.rb
    new file:   spec/spec_helper.rb

---
+++ lib/catalog/of/things/version.rb
@@ -0,0 +1,7 @@
+module Catalog
+  module Of
+    module Things
+      VERSION = '0.1.0'.freeze
+    end
+  end
+end
---
+++ spec/catalog/of/things_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Catalog::Of::Things do
+  it 'has a version number' do
+    expect(Catalog::Of::Things::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
