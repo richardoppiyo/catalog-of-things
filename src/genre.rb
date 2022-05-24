6e241d92269caba950d9774d1b185012aca35ec6
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-05-24 10:22:53 -0500

    Add Genre and MusicAlbum classes
    new file:   src/genre.rb
    new file:   src/music_album.rb

---
+++ src/genre.rb
@@ -0,0 +1,40 @@
+require_relative './item'
+
+class Genre
+  def initialize(id, name)
+    @id = id
+    @name = name
+    @items = []
+  end
+  attr_reader :name
+  attr_accessor :items
+
+  def add_item(item)
+    @items.push item
+    item.add_genre(self)
+  end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'data' => [@id, @name],
+      'items' => [@items]
+    }.to_json(*args)
+  end
+
+  def self.json_create(object)
+    puts "Data:#{object['data']}"
+    puts "Items:#{object['items']}"
+    genre = new(*object['data'])
+    genre.items = object['items'][0]
+    genre.update_associations
+    genre
+  end
+
+  private
+
+  def update_associations
+    @items.each { |item| item.add_genre(self) }
+  end
+
+end
