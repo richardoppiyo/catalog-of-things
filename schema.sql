40b2a39a0a4bb9c38528548d3adb87fa7fbccf72
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-05-25 11:23:18 -0500

    Update schema to include constraints inside the Create Table
    modified:   schema.sql

--- schema.sql
+++ schema.sql
@@ -1,23 +1,18 @@
 -- Create table genre
-DROP TABLE IF EXISTS "genre";
-CREATE TABLE "genre"(
-    "id" INTEGER NOT NULL,
-    "name" VARCHAR(255) NOT NULL
+DROP TABLE IF EXISTS genre;
+CREATE TABLE genre(
+    id INTEGER NOT NULL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
 );
-ALTER TABLE
-    "genre" ADD PRIMARY KEY("id");
 
 -- Create table music_album
-DROP TABLE IF EXISTS  "music_album";
-CREATE TABLE "music_album"(
-    "id" INTEGER NOT NULL,
-    "genre" INTEGER NOT NULL,
-    "publish_date" DATE NOT NULL,
-    "archived" BOOLEAN NOT NULL,
-    "on_spotify" BOOLEAN NOT NULL
+DROP TABLE IF EXISTS  music_album;
+CREATE TABLE music_album(
+    id INTEGER NOT NULL PRIMARY KEY,
+    genre INTEGER NOT NULL,
+    publish_date DATE NOT NULL,
+    archived BOOLEAN NOT NULL,
+    on_spotify BOOLEAN NOT NULL,
+    FOREIGN KEY (genre)
+        REFERENCES genre (id)
 );
-
-ALTER TABLE
-    "music_album" ADD PRIMARY KEY("id");
-ALTER TABLE
-    "music_album" ADD CONSTRAINT "music_album_genre_foreign" FOREIGN KEY("genre") REFERENCES "genre"("id");
