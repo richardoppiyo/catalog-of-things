248c11abcd9b50074cc0834ee69997b4626d316c
Richard Opiyo <richardoppiyo@gmail.com>
2022-05-25 23:25:15 +0300

    fix linter errors

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -4,7 +4,7 @@ require 'json'
 
 RSpec.describe Label do
   before :each do
-    @label = Label.new(2,'Science Fictions','Red')
+    @label = Label.new(2, 'Science Fictions', 'Red')
   end
 
   it 'Create an instance of Label' do
@@ -12,16 +12,16 @@ RSpec.describe Label do
   end
 
   it 'Add items to labels' do
-    book1 = Book.new('2013/03/12',true,'richard','rocky',4846)
+    book1 = Book.new('2013/03/12', true, 'richard', 'rocky', 4846)
     @label.add_items(book1)
     json = JSON.generate(@label)
     expect(json).to be == '{"json_class":"Label","data":[2,"Science Fictions","Red"],'         \
-                            '"items":[[{"json_class":"Book","data":'\
-                            '["2013/03/12",true,"richard","rocky",4846]}]]}'
+                          '"items":[[{"json_class":"Book","data":'\
+                          '["2013/03/12",true,"richard","rocky",4846]}]]}'
   end
 
   it 'Create empty label from JSON' do
-    label = Label.new(1,'Algorithms','Green')
+    label = Label.new(1, 'Algorithms', 'Green')
     json = JSON.generate(label)
     expect(json).to be == '{"json_class":"Label","data":[1,"Algorithms","Green"],"items":[[]]}'
     label = JSON.parse(json, create_additions: true)
